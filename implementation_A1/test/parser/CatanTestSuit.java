d2b0befe306382b186d7e875f057b6b4c742c252
Delvin <delvinafkhami@gmail.com>
2026-03-08 03:31:49 -0400

    Create CatanTestSuit.java (#20)
    What did I change? created a Test suite
    Why did I change it? run the test classes together
    What files/classes I touched? created CatanTestSuite.java
    Risks or anything unfinished? n/a
    Test method(If been done): jnit tests

---
+++ implementation_A1/test/parser/CatanTestSuit.java
@@ -0,0 +1,46 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+//=========================
+//CatanTestSuite.java
+//run all test classes together as a suite
+//=========================
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        //run in the order below
+        PlayerTests.class,
+        ResourcesTests.class,
+        DiceTests.class,
+        TileTests.class,
+        BoardTests.class,
+        ResourceProductionTests.class,
+})
+
+public class CatanTestSuite {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
